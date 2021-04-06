7e40e5c03f609fef028cd3f6faec0212dd0e14d7
abonilla111git config --global user.name abonilla111 <antoniobonila71@gmail.comgit config --global user.email antoniobonila71@gmail.com>
2021-04-06 23:39:05 +0000

    variables.py

---
+++ variables.py
@@ -0,0 +1,22 @@
+# Variables.py
+# Author: Antonio Bonilla
+# March 26, 2021
+
+year = 2021;
+sentence = "My name is Antonio";
+sarah, bob, mike, = 16, 20, 25;
+name, age = "maria", 24;
+mary = fer = 20;
+PI = 3.1416;
+e = 2.71; 
+
+# display values
+
+print "year = ", year
+print sentence
+print "sarah :", sarah, "\nbob : ", bob, "\nmike : ", mike
+print "\n\t name: ", name
+print "\n\t age: ", age
+print mary, fer
+print PI
+print e 
