3d425ca0bf1775ba990b24e26eda35ce22b32d0b
abonilla111 <antoniobonila71@gmail.com>
2021-04-20 15:09:52 -0500

    add examen-demo1

---
+++ examen-demo1/datos.py
@@ -0,0 +1,10 @@
+# Antonio Bonilla
+# datos.py
+
+nombre = "Antonio Bonilla"
+matricula = "zs18021355"
+carrera = "LSCA"
+
+message = " Nombre: %s \n Matricula: %s \n Carrera: %s"
+print (message%(nombre, matricula, carrera))
+
---
+++ examen-demo1/mydict.py
@@ -0,0 +1,11 @@
+#Antonio Bonilla
+#mydict.py
+
+carreras = {
+        "LSCA":"lic en sistemas computacionales administrativos",
+        "IS":"ingenieria de software",
+        "LAE":"lic en administracion de empresas"
+        }
+
+print (carreras)
+print ("longitud: " ,len(carreras))
